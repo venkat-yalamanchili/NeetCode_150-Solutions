642793d1d17f2a1a87cbc51cd4bbf95074bea786
venkat-yalamanchili <venkatyalamanchili56@gmail.com>
2026-03-05 15:24:07 -0800

    1st problem

---
+++ 1.ContainsDuplicate.py
@@ -0,0 +1,7 @@
+def hasDuplicate(self, nums: list[int]) -> bool:
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
